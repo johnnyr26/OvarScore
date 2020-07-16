85bca3ccbd7adba42dd9f501dda8e4b2f9b593f3
JRProgramming <ramirezjohnny@icloud.com>
2020-07-16 15:09:18 -0400

    Add ejs to the application

--- routes/app.js
+++ routes/app.js
@@ -20,132 +20,114 @@ app.use(session({
     saveUninitialized: false
 }));
 app.use(helmet());
+app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
-    fs.readFile('views/index.html', {encoding: 'utf-8'}, (error, body)  => {
-        if(error) return res.status(404).send('404');
-        if(!req.session.user) return res.redirect('/login');
-        delete req.session.recommendation;
-        delete req.session.responses;
-        return res.send(body.replace('{{name}}', req.session.name));
+    if(!req.session.user) return res.redirect('/login');
+    delete req.session.recommendation;
+    delete req.session.responses;
+    return res.render('index', {
+        name: req.session.name
     });
 });
 app.get('/ago', (req, res) => {
-    fs.readFile('views/ago.html', {encoding: 'utf-8'}, (error, body)  => {
-        if(error) return res.status(404).send('404');
-        if(!req.session.user) return res.redirect('/login');
-        return res.send(body);
-    });
+    if(!req.session.user) return res.redirect('/login');
+    return res.render('ago');
 });
 app.get('/imodel', (req,res) => {
-    fs.readFile('views/imodel.html', {encoding: 'utf-8'}, (error, body) => {
-        if(error) return res.status(404).send('404');
-        if(!req.session.user) return res.redirect('/login');
-        return error ? res.status(404).send('404') : res.send(body);
-    });
+    if(!req.session.user) return res.redirect('/login');
+    return res.render('imodel');
 });
 app.get('/fagotti', (req, res) => {
-    fs.readFile('views/fagotti.html', {encoding: 'utf-8'}, (error, body) => {
-        if(error) return res.status(404).send('404');
-        if(!req.session.user) return res.redirect('/login');
-        return res.send(body);
-    });
+    if(!req.session.user) return res.redirect('/login');
+    return res.render('fagotti');
 });
 app.get('/login', (req, res) => {
-    fs.readFile('views/login.html', {encoding: 'utf-8'}, (error, body) => {
-        if(error) return res.status(404).send('404');
-        if(req.session.error) {
-            const error = req.session.error;
-            delete req.session.error;
-            return res.send(body.replace('{{Error Message}}', error));
-        }
-        delete req.session.user;
-        return res.send(body.replace('{{Error Message}}', ''));
-    })
+    if(req.session.error) {
+        const error = req.session.error;
+        delete req.session.error;
+        return res.render('login', {
+            error: error
+        })
+    }
+    delete req.session.user;
+    return res.render('login');
 })
 app.get('/logout', (req, res) => {
     const categories = ['name', 'email', 'user', 'recommendation', 'responses', 'error'];
     categories.forEach(category => delete req.session[category]);
-    return res.redirect('/');
+    return res.redirect('/login');
 })
 app.get('/signup', (req, res) => {
-    fs.readFile('views/signup.html', {encoding: 'utf-8'}, (error, body) => {
-        if(error) return res.status(404).send('404');
-        if(req.session.error) {
-            const name = req.session.name.trim();
-            const email = req.session.email.trim();
-            const error = req.session.error;
-            delete req.session.name;
-            delete req.session.email;
-            delete req.session.error;
-            return res.send(body.replace('{{Error Message}}', error).replace('{name}', name).replace('{email}', email));
-        }
-        return res.send(body.replace('{{Error Message}}', '').replace('{name}', '').replace('{email}', ''));
-    });
+    if(req.session.error) {
+        const name = req.session.name.trim();
+        const email = req.session.email.trim();
+        const error = req.session.error;
+        delete req.session.name;
+        delete req.session.email;
+        delete req.session.error;
+        console.log('aslkdjflasdkjf')
+        return res.render('signup', {
+            error: error,
+            name: name,
+            email: email
+        })
+    }
+    console.log('basjdfoajsd')
+    return res.render('signup');
 });
 app.get('/*', (req, res) => {
-    fs.readFile('views/404.html', {encoding: 'utf-8'}, (error, body) => {
-        if(error) return res.status(404).send('404');
-        return res.send(body);
-    })
-})
-app.post('/login', (req,res) => {
-    fs.readFile('views/login.html', {encoding: 'utf-8'}, async (error, body) => {
-        if(error) {
-            req.session.error = "There was an error authenticating your account. Plese try again";
-            return res.redirect('/login');
-        };
-        const email = req.body.username;
-        const password = req.body.password;
-        try {
-            const hashedPassword = await user.findPassword(email);
-            bcrypt.compare(password, hashedPassword, function(error, result) {
-                if(error) {
-                    req.session.error = "There was an error authenticating your account. Plese try again";
-                    return res.redirect('/login');
-                }
-                if(result) {
-                    req.session.user = email;
-                    req.session.name = user.users.find(user => user['email'] === email).name;
-                    return res.redirect('/');
-                }
-                req.session.error = 'Incorrect password. Please try again.';
-                return res.redirect('/login')
-            });
-        } catch(error) {
-            req.session.error = error; 
-            return res.redirect('/login');
-        }
-    });
+    return res.render('404');
 });
-app.post('/signup', (req,res) => {
-    fs.readFile('views/signup.html', {encoding: 'utf-8'}, async (error, body) => {
-        if(error) return res.status(404).send('404');
-        //If all of the entries were in the correct format and everything is confirmed
-        try {
-            await bcrypt.hash(req.body['password'], saltRounds, function(error, hashedPassword) {
-                if(error) {
-                    req.session.name = req.body['name'];
-                    req.session.email = req.body['email'];
-                    req.session.error = "Error: The sign up credentials couldn't reach the database. Please try again.";
-                    return res.redirect('/signup');
-                }
-                req.body['password'] = hashedPassword;
-            });
-            user.signUpCredentials = req.body;
-            await user.validateSignUpCredentials();
-            req.session.user = user.email;
-            req.session.name = user.name;
-            delete user.name;
-            delete user.email;
-            delete user.password;
-            return res.redirect('/');
-        } catch (error) {
-            req.session.name = user.name;
-            req.session.email = user.email;
-            req.session.error = error;
-            return res.redirect('/signup');
-        }
-    });
+app.post('/login', async (req,res) => {
+    const email = req.body.username;
+    const password = req.body.password;
+    try {
+        const hashedPassword = await user.findPassword(email);
+        bcrypt.compare(password, hashedPassword, function(error, result) {
+            if(error) {
+                req.session.error = "There was an error authenticating your account. Plese try again";
+                return res.redirect('/login');
+            }
+            if(result) {
+                req.session.user = email;
+                req.session.name = user.users.find(user => user['email'] === email).name;
+                return res.redirect('/');
+            }
+            req.session.error = 'Incorrect password. Please try again.';
+            return res.redirect('/login')
+        });
+    } catch(error) {
+        req.session.error = error; 
+        return res.redirect('/login');
+    }
+});
+app.post('/signup', async (req,res) => {
+    //If all of the entries were in the correct format and everything is confirmed
+    try {
+        await bcrypt.hash(req.body['password'], saltRounds, function(error, hashedPassword) {
+            if(error) {
+                req.session.name = req.body['name'];
+                req.session.email = req.body['email'];
+                req.session.error = "Error: The sign up credentials couldn't reach the database. Please try again.";
+                return res.redirect('/signup');
+            }
+            req.body['password'] = hashedPassword;
+        });
+        user.signUpCredentials = req.body;
+        await user.validateSignUpCredentials();
+        req.session.user = user.email;
+        req.session.name = user.name;
+        delete user.name;
+        delete user.email;
+        delete user.password;
+        return res.redirect('/');
+    } catch (error) {
+        req.session.name = user.name;
+        req.session.email = user.email;
+        req.session.error = error;
+        console.log('error')
+        return res.redirect('/signup');
+    }
 });
 app.post('/ago', async (req, res) => {
     //Retrieves the response to each category
