27ea6dfff5bd19f487b7f3482ab3aaaea5e0fc58
Johnny Ramirez <ramirezjohnny@icloud.com>
2020-08-13 16:56:04 -0400

    Add automatic no surgery for mesentric

--- docs/js/fagotti.js
+++ docs/js/fagotti.js
@@ -36,10 +36,20 @@ var processResponse = function processResponse(responses) {
     'Any nodules >2 cm'
   ];
   var score = 0;
+  var recommendation = '';
+  var automaticNoSurgery = false;
   Object.values(responses).forEach(function (response) {
+    if (response === 'Large infiltrating nodules or involvement of the root of the mesentery assumed based on limited movements of various intestinal segments') {
+      automaticNoSurgery = true;
+    }
     if (twoCategory.includes(response)) score += 2;
   });
-  var recommendation = '';
+  if (automaticNoSurgery) {
+    return {
+      score: null,
+      recommendation: 'Recommendation: No surgery'
+    }
+  }
   var everyCategoryFilled = Object.values(responses).length === 7;
   if (everyCategoryFilled) recommendation = score <= 10 ? 'Recommendation: Surgery' : 'Recommendation: No Surgery';
   return {
@@ -71,8 +81,12 @@ Object.values(document.getElementsByTagName('td')).forEach(function (cell) {
     } else {
       var response = processResponse(input);
       document.getElementById('recommendation').textContent = response.recommendation;
-      document.getElementById('score').textContent = 'Score: ' + response.score;
-
+      // Null since 0 is considered a falsey value
+      if (response.score !== null) {
+        document.getElementById('score').textContent = 'Score: ' + response.score;
+      } else {
+        document.getElementById('score').textContent = '';
+      }
       if (response.recommendation) {
         window.scroll({
           top: document.body.scrollHeight,
