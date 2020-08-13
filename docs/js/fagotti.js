68d62d0935b8aa09e6604e18fb7498204ecd31f0
Johnny Ramirez <ramirezjohnny@icloud.com>
2020-08-13 11:28:28 -0400

    Update Fagotti table and fix typos

--- docs/js/fagotti.js
+++ docs/js/fagotti.js
@@ -1,8 +1,24 @@
 "use strict";
 
 var validate = function validate(responses) {
-  var twoCategory = ['Unresectable massive peritoneal involvement plus miliary pattern of distribution', 'Widespread infiltrating carcinomatosis or confluent nodules to most of the diaphragmatic surface', 'Large infiltrating nodules or involvement of the root of the mesentery assumed based on limited movements of various intestinal segments', 'Tumour diffusion up to the large curvature of the stomach', 'Bowel resection assumed to be required or military carcinomatosis at the mesentric junction', 'Obvious neoplasyic involvement of the gastric wall', 'Any nodules >2 cm'];
-  var zeroCategory = ['Carcinomatosis involving a limited area surgically removable by peritonectomy', 'Isolated diaphragmatic disease', 'Small nodules potentially treatable with argon-beam coagulation', 'Isolated omental disease', 'No bowel restriction required and no military carcinomatosis at the mesenteric junction', 'No obvious neoplastic involvement of the gastric wall', 'No surface lesions'];
+  var twoCategory = [
+    'Unresectable massive peritoneal involvement plus miliary pattern of distribution', 
+    'Widespread infiltrating carcinomatosis or confluent nodules to most of the diaphragmatic surface', 
+    'Large infiltrating nodules or involvement of the root of the mesentery assumed based on limited movements of various intestinal segments', 
+    'Tumour diffusion up to the large curvature of the stomach', 
+    'Possible large/small bowel resection (excluding, recto-sigmoid involvement) assumed to be required or miliary carcinomatosis at mesenteric junction', 
+    'Obvious neoplastic involvement of the stomach, and/or lesser omentum, and/or spleen',
+    'Any nodules >2 cm'
+  ];
+  var zeroCategory = [
+    'Carcinomatosis involving a limited area surgically removable by peritonectomy', 
+    'Isolated diaphragmatic disease', 
+    'Small nodules potentially treatable with argon-beam coagulation', 
+    'Isolated omental disease', 
+    'No bowel resection required and no miliary carcinomatosis at the mesenteric junction', 
+    'No obvious neoplastic involvement of the stomach, and/or lesser omentum, and/or spleen', 
+    'No nodules >2 cm'
+  ];
   var correctValues = Object.values(responses).every(function (response) {
     return !response || twoCategory.includes(response) || zeroCategory.includes(response);
   });
@@ -10,7 +26,15 @@ var validate = function validate(responses) {
 };
 
 var processResponse = function processResponse(responses) {
-  var twoCategory = ['Unresectable massive peritoneal involvement plus miliary pattern of distribution', 'Widespread infiltrating carcinomatosis or confluent nodules to most of the diaphragmatic surface', 'Large infiltrating nodules or involvement of the root of the mesentery assumed based on limited movements of various intestinal segments', 'Tumour diffusion up to the large curvature of the stomach', 'Bowel resection assumed to be required or military carcinomatosis at the mesentric junction', 'Obvious neoplasyic involvement of the gastric wall', 'Any nodules >2 cm'];
+  var twoCategory = [
+    'Unresectable massive peritoneal involvement plus miliary pattern of distribution', 
+    'Widespread infiltrating carcinomatosis or confluent nodules to most of the diaphragmatic surface', 
+    'Large infiltrating nodules or involvement of the root of the mesentery assumed based on limited movements of various intestinal segments', 
+    'Tumour diffusion up to the large curvature of the stomach', 
+    'Possible large/small bowel resection (excluding, recto-sigmoid involvement) assumed to be required or miliary carcinomatosis at mesenteric junction', 
+    'Obvious neoplastic involvement of the stomach, and/or lesser omentum, and/or spleen',
+    'Any nodules >2 cm'
+  ];
   var score = 0;
   Object.values(responses).forEach(function (response) {
     if (twoCategory.includes(response)) score += 2;
