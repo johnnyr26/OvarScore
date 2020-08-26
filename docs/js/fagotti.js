6c992f6c4758b2aae152b2bcf269f7d369895978
Johnny Ramirez <ramirezjohnny@icloud.com>
2020-08-26 13:28:40 -0400

    Make one last update with Fagotti page
    Please let this be the last commit

--- docs/js/fagotti.js
+++ docs/js/fagotti.js
@@ -1,63 +1,104 @@
 "use strict";
 
-var validate = function validate(responses) {
-  var twoCategory = [
-    'Unresectable massive peritoneal involvement plus miliary pattern of distribution', 
-    'Widespread infiltrating carcinomatosis or confluent nodules to most of the diaphragmatic surface', 
-    'Large infiltrating nodules or involvement of the root of the mesentery assumed based on limited movements of various intestinal segments', 
-    'Tumour diffusion up to the large curvature of the stomach', 
-    'Possible large/small bowel resection (excluding, recto-sigmoid involvement) assumed to be required or miliary carcinomatosis at mesenteric junction', 
-    'Obvious neoplastic involvement of the stomach, and/or lesser omentum, and/or spleen',
-    'Any nodules >2 cm'
-  ];
-  var zeroCategory = [
-    'Carcinomatosis involving a limited area surgically removable by peritonectomy', 
-    'Isolated diaphragmatic disease', 
-    'Small nodules potentially treatable with argon-beam coagulation', 
-    'Isolated omental disease', 
-    'No bowel resection required and no miliary carcinomatosis at the mesenteric junction', 
-    'No obvious neoplastic involvement of the stomach, and/or lesser omentum, and/or spleen', 
-    'No nodules >2 cm'
-  ];
-  var correctValues = Object.values(responses).every(function (response) {
-    return !response || twoCategory.includes(response) || zeroCategory.includes(response);
-  });
-  return correctValues;
-};
-
-var processResponse = function processResponse(responses) {
-  var twoCategory = [
-    'Unresectable massive peritoneal involvement plus miliary pattern of distribution', 
-    'Widespread infiltrating carcinomatosis or confluent nodules to most of the diaphragmatic surface', 
-    'Large infiltrating nodules or involvement of the root of the mesentery assumed based on limited movements of various intestinal segments', 
-    'Tumour diffusion up to the large curvature of the stomach', 
-    'Possible large/small bowel resection (excluding, recto-sigmoid involvement) assumed to be required or miliary carcinomatosis at mesenteric junction', 
-    'Obvious neoplastic involvement of the stomach, and/or lesser omentum, and/or spleen',
-    'Any nodules >2 cm'
-  ];
-  var score = 0;
-  var recommendation = '';
-  var automaticNoSurgery = false;
-  Object.values(responses).forEach(function (response) {
-    if (response === 'Large infiltrating nodules or involvement of the root of the mesentery assumed based on limited movements of various intestinal segments') {
-      automaticNoSurgery = true;
-    }
-    if (twoCategory.includes(response)) score += 2;
-  });
-  if (automaticNoSurgery) {
-    return {
-      score: null,
-      recommendation: 'Recommendation: No surgery'
-    }
+var validate = function validate(step, responses) {
+  if (step === 1) {
+    var categories = ['retraction', 'serosa', 'junction', 'bowel'];
+    var validatedKeys = Object.keys(responses).every(key => categories.includes(key));
+    var validatedValues = Object.values(responses).every(value => value === 'Yes' || value === 'No');
+    return validatedKeys && validatedValues;
+  } else {
+    var twoCategory = [
+      'Unresectable massive peritoneal involvement plus miliary pattern of distribution', 
+      'Widespread infiltrating carcinomatosis or confluent nodules to most of the diaphragmatic surface', 
+      'Large infiltrating nodules or involvement of the root of the mesentery assumed based on limited movements of various intestinal segments', 
+      'Tumour diffusion up to the large curvature of the stomach', 
+      'Possible large/small bowel resection (excluding, recto-sigmoid involvement) assumed to be required or miliary carcinomatosis at mesenteric junction', 
+      'Obvious neoplastic involvement of the stomach, and/or lesser omentum, and/or spleen',
+      'Any nodules >2 cm'
+    ];
+    var zeroCategory = [
+      'Carcinomatosis involving a limited area surgically removable by peritonectomy', 
+      'Isolated diaphragmatic disease', 
+      'Small nodules potentially treatable with argon-beam coagulation', 
+      'Isolated omental disease', 
+      'No bowel resection required and no miliary carcinomatosis at the mesenteric junction', 
+      'No obvious neoplastic involvement of the stomach, and/or lesser omentum, and/or spleen', 
+      'No nodules >2 cm'
+    ];
+    var correctValues = Object.values(responses).every(function (response) {
+      return !response || twoCategory.includes(response) || zeroCategory.includes(response);
+    });
+    return correctValues;
   }
-  var everyCategoryFilled = Object.values(responses).length === 7;
-  if (everyCategoryFilled) recommendation = score <= 10 ? 'Recommendation: Surgery' : 'Recommendation: No Surgery';
-  return {
-    score: score,
-    recommendation: recommendation
-  };
 };
 
+var processResponse = function processResponse(step, responses) {
+  if (step === 1) {
+    return Object.values(responses).every(response => response === 'Yes');
+  } else {
+    var twoCategory = [
+      'Unresectable massive peritoneal involvement plus miliary pattern of distribution', 
+      'Widespread infiltrating carcinomatosis or confluent nodules to most of the diaphragmatic surface', 
+      'Large infiltrating nodules or involvement of the root of the mesentery assumed based on limited movements of various intestinal segments', 
+      'Tumour diffusion up to the large curvature of the stomach', 
+      'Possible large/small bowel resection (excluding, recto-sigmoid involvement) assumed to be required or miliary carcinomatosis at mesenteric junction', 
+      'Obvious neoplastic involvement of the stomach, and/or lesser omentum, and/or spleen',
+      'Any nodules >2 cm'
+    ];
+    var score = 0;
+    var recommendation = '';
+    var automaticNoSurgery = false;
+    Object.values(responses).forEach(function (response) {
+      if (response === 'Large infiltrating nodules or involvement of the root of the mesentery assumed based on limited movements of various intestinal segments') {
+        automaticNoSurgery = true;
+      }
+      if (twoCategory.includes(response)) score += 2;
+    });
+    if (automaticNoSurgery) {
+      return {
+        score: null,
+        recommendation: 'Recommendation: No surgery'
+      }
+    }
+    var everyCategoryFilled = Object.values(responses).length === 7;
+    if (everyCategoryFilled) recommendation = score <= 10 ? 'Recommendation: Surgery' : 'Recommendation: No Surgery';
+    return {
+      score: score,
+      recommendation: recommendation
+    };
+  }
+};
+Object.values(document.querySelectorAll('input[type="radio"]')).forEach(function (element) {
+  element.addEventListener('click', function  () {
+    var radios = document.querySelectorAll('input[type="radio"]');
+    var input = {};
+    radios.forEach(radio => {
+      if (radio.checked) {
+        input[radio.name] = radio.value;
+      }
+    })
+    if (!validate(1, input)) {
+      document.getElementById('recommendation').textContent = 'Error: There was an error processing the response.';
+      document.getElementById('fagotti-main-div').style.display = 'none';
+      return window.scroll({
+        top: document.body.scrollHeight,
+        behavior: 'smooth'
+      });
+    }
+    document.getElementById('recommendation').textContent = '';
+    if (Object.values(input).some(response => response !== 'Yes')) {
+      document.getElementById('recommendation').textContent = 'Recommendation: No surgery';
+      document.getElementById('fagotti-main-div').style.display = 'none';
+      return window.scroll({
+        top: document.body.scrollHeight,
+        behavior: 'smooth'
+      });
+    } else if (Object.entries(input).length === 4 && processResponse(1, Object.values(input))) {
+      document.getElementById('fagotti-main-div').style.display = 'block';
+      document.getElementById("fagotti-main-div").scrollIntoView({behavior: 'smooth'});
+    }
+  });
+});
 Object.values(document.getElementsByTagName('td')).forEach(function (cell) {
   cell.addEventListener('click', function () {
     var classCells = document.getElementsByClassName(cell.className);
@@ -75,11 +116,11 @@ Object.values(document.getElementsByTagName('td')).forEach(function (cell) {
         input['row' + Math.floor(index / 2)] = cell.textContent;
       }
     });
-    if (!validate(input)) {
+    if (!validate(2, input)) {
       document.getElementById('recommendation').textContent = 'Error: There was an error processing the response.';
       document.getElementById('score').textContent = '';
     } else {
-      var response = processResponse(input);
+      var response = processResponse(2, input);
       document.getElementById('recommendation').textContent = response.recommendation;
       // Null since 0 is considered a falsey value
       if (response.score !== null) {
